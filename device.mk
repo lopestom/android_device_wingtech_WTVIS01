26d9a97909d1f7528364504fe5e44821e1d712ee
lopestorm <guillopes2002@gmail.com>
2023-03-08 00:02:25 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,11 +6,34 @@
 #
 
 LOCAL_PATH := device/wingtech/WTVIS01
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+PRODUCT_PLATFORM := mt6765
+
 # A/B
+ENABLE_VIRTUAL_AB := true
+AB_OTA_UPDATER := true
+
 AB_OTA_PARTITIONS += \
     boot \
+    dtbo \
+    lk \
+    preloader \
+    odm \
+    product \
     system \
-    vendor
+    vbmeta \
+    vbmeta_vendor \
+    vbmeta_system \
+    vendor \
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -20,21 +43,26 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.1-impl-recovery \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.mt6765
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6765 \
-    libgptutils \
-    libz \
-    libcutils
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
-    cppreopts.sh \
     update_engine \
     update_verifier \
     update_engine_sideload
+    
+# Fastbootd stuff
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service \
